af391aa3b96e6b379b1bb3b63171ac303d647542
ArtemZemlyakov <139268188+ArtemZemlyakov@users.noreply.github.com>
2023-07-15 10:35:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const favoriteSport = 'football';
+console.log(favoriteSport);
+console.log(typeof 'football');
+
+const numberOfTeams = '1';
+console.log(numberOfTeams);
+console.log(typeof 1);
+
+const activSports = true;
+console.log(activSports);
+console.log(typeof true);
+
+const womensFavoriteSport = null;
+console.log(womensFavoriteSport);
+console.log(typeof Symbol('id'));
+
+let clubOwner;
+console.log(clubOwner);
+console.log(typeof undefined);
+
+const sport = {
+    favoriteSport: 'football',
+    numberOfTeams: 1,
+    activSports: true,
+
+};
+console.log(sport);
+console.log(typeof console);
+
+const id = Symbol('id');
+console.log(id);
+
+const bigInNumber = BigInt(10);
+console.log(bigInNumber);
+console.log(typeof 100n);
+
+
+
+
